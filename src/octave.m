38b20047f43def2ca155306e38e2d7c121179f73
gabrieldbs <gabriel.dbs.1@gmail.com>
2018-11-07 14:21:00 -0300

    Update octave.m

--- src/octave.m
+++ src/octave.m
@@ -1,39 +1,37 @@
-// Funciones octave
-// 
-//1
+%// Funciones octave
+%// 
+%//1
 function f_ha(f_a,rho_h_mas,ka)
-	f_ha=rho_h_mas*fa/ka
+	f_ha=rho_h_mas*f_a/ka
 endfunction
 
-//2
+%//2
 function f_hap(f_a,rho_h_mas,ka)
 	f_hap=1-f_a-rho_h_mas*f_a/ka;
 endfunction
 
-//3
+%//3
 function f_eop(f_a,rho_h_mas,ka)
 	f_eop= 1-f_a-rho_h_mas*f_a/ka;
 endfunction
 
-//4
+%//4
 function f_eo(f_a,rho_h_mas,ka)
 	f_eo=f_a*(1+rho_h_mas/ka);
 endfunction
-//5
+%//5
 function rho_h_mas(psi,beta,rho_h,qh)
 	rho_h_mas=rho_h*exp(-beta*psu*qh)
 endfunction
-//6
+%//6
 function rho_a_menos(psi,beta,rho_a,qa)
 	rho_a_menos=rho_a*exp(-beta*psi*q_a)
 endfunction
-//ecuacion1
+%//ecuacion1
 function ecuacion1(f_a,psi,ka,beta,q_h,kd,rho_o,rho_h)
 	ecuacion1=f_a*(1+rho_h*(exp(-beta*psi*q_h))/ka)+(kd/ka)*rho_o*f_a*rho_h*(exp(-beta*psi*qh))*(f_a+f_a*rho_h*(exp(-beta*psi*qh))/ka)
 endfunction
-//ecuacion2
+%//ecuacion2
 function ecuacion2(f_a,psi,beta,qa,qh,rho_a,rho_h,rho_o)
 	ecuacion2=rho_a*exp(-beta*psi*q_a)+rho_of_a+rho_h*exp(-beta*psi*q_h)
 endfunction
-
-
