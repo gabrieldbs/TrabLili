061c39caf0e1b677bda19c948a65c9b3703f14bb
gabrieldbs <gabriel.dbs.1@gmail.com>
2018-11-09 11:46:05 -0300

    Update funcion_2.m

--- src/funcion_2.m
+++ src/funcion_2.m
@@ -1,27 +1,32 @@
 function F2 = funcion_2(f_a,ps_i)
   %%-----------------------
   % LA IDEA ES QUE F2 SEA UN ARRAY DE 8 VARIABLES RESUELTAS,
-  %F2=[f_a,f_ha,f_hap,f_eo,f_eop,rho_h_mas,rho_a_menos,psi]
+  %F2=[f_a,psi,rho_h_mas,rho_a_menos,f_ha,f_hap,f_eo,f_eop]
   %%-------CONSTANTES-----------------%
-  
-  
+  ka=1;
+  bet_a=1;
+  q_h=1;
+  q_a=1;
+  rho_h=1;
+  rho_a=1; 
   %-----------------VARIABLES---------------%
+  
   F2(1)=f_a;
-  f_ha = @(f_a,rho_h_mas,ka) rho_h_mas*f_a/ka;
-  F2(2)=f_ha;
-  f_hap = @(f_a,rho_h_mas,ka) 1-f_a-rho_h_mas*f_a/ka;
-  F2(3)=f_hap;
+  F2(2)=ps_i;
+  rho_h_mas= rho_h*exp(-bet_a*ps_i*q_h);
+  F2(3)=rho_h_mas;
+  rho_a_menos= rho_a*exp(-bet_a*ps_i*q_a);
+  F2(4)=rho_h_menos;
   
-  f_eo= @(f_a,rho_h_mas,ka) f_a*(1+rho_h_mas/ka);
-  F2(4)=f_eo;
-  f_eop = @(f_a,rho_h_mas,ka) 1-f_a-rho_h_mas*f_a/ka;
-  F2(5)=f_eop;
+  f_ha = rho_h_mas*f_a/ka;
+  F2(5)=f_ha;
+  f_hap = 1-f_a-rho_h_mas*f_a/ka;
+  F2(6)=f_hap;
   
-  rho_h_mas= @(ps_i,bet_a,rho_h,q_h) rho_h*exp(-bet_a*ps_i*q_h);
-  F2(6)=rho_h_mas;
-  rho_a_menos=@(ps_i,bet_a,rho_a,q_a) rho_a*exp(-bet_a*ps_i*q_a);
-  F2(7)=rho_h_menos;
+  f_eo= f_a*(1+rho_h_mas/ka);
+  F2(7)=f_eo;
+  f_eop = 1-f_a-rho_h_mas*f_a/ka;
+  F2(8)=f_eop;
   
-  F2(8)=ps_i;
  endfunction;
  
