2e9d3dbd5652321fe8e2aeb87e9272988678d5f4
gabrieldbs <gabriel.dbs.1@gmail.com>
2018-11-12 10:03:22 -0300

    Update funcion_3.m

--- src/funcion_3.m
+++ src/funcion_3.m
@@ -1,5 +1,5 @@
-function F3= funcion_3(Y,rho_o)
-%Y=[f_a (1),psi (2),rho_h_mas (3) ,rho_a_menos (4) ,f_ha (5) ,f_hap (6),f_eo (7),f_eop(8)]
+function F3= funcion_3(Z)
+%Y=[f_a (1),psi (2),rho_h_mas (3) ,rho_a_menos (4) ,f_ha (5) ,f_hap (6),f_eo (7),f_eop(8),rho_o (9)]
 %-----------------------------CONSTANTES--------------%
 v_a_menos=1;
 muo_a_menos=1;
@@ -23,17 +23,17 @@ q=h=1;
 %----------------------------------------------%  
   EnLi_2= Y(3)*log(Y(3)*v_h_mas)-1+muo_h_mas;
 %---------------------------------------%
-  EnLi_3= rho_o*( Y(5)*log(Y(5))+ Y(1)*log(Y(1)) +Y(6)*log(Y(6)));
+  EnLi_3= Z(9)*( Y(5)*log(Y(5))+ Y(1)*log(Y(1)) +Y(6)*log(Y(6)));
 %---------------------------------------%
-  EnLi_4=  rho_o*( Y(7)*log(Y(7)) +Y(8)*log(Y(8)) +Y(7)*bet_a*muo_eo);
+  EnLi_4=  Z(9)*( Y(7)*log(Y(7)) +Y(8)*log(Y(8)) +Y(7)*bet_a*muo_eo);
 %---------------------------------------%
-  EnLi_5= rho_o*Y(8)*( bet_a+muo_eop-log(rho_o*Y(8)+v_pair)+ 1 );
+  EnLi_5= Z(9)*Y(8)*( bet_a+muo_eop-log(Z(9)*Y(8)+v_pair)+ 1 );
 %---------------------------------------%
-  EnLi_6= rho_o* (Y(5)*muo_ha+Y(1)*muo_a );
+  EnLi_6= Z(9)* (Y(5)*muo_ha+Y(1)*muo_a );
 %---------------------------------------%
-  EnLi_7=  Y(4)*mu_a_menos+Y(3)*mu_h_mas+mu_h*Y(5)*rho_o;
+  EnLi_7=  Y(4)*mu_a_menos+Y(3)*mu_h_mas+mu_h*Y(5)*Z(9);
 %---------------------------------------%
-  EnLi_8=  Y(2)*(Y(4)*q_a+Y(3)*q_h+rho_o*Y(1)*q_a );
+  EnLi_8=  Y(2)*(Y(4)*q_a+Y(3)*q_h+Z(9)*Y(1)*q_a );
 
 %------------------------------------------------------------------------------%
   EnLiTot=EnLi_1EnLi_2+EnLi_3+EnLi_4+EnLi_5+EnLi_6+EnLi_7+EnLi_8;
