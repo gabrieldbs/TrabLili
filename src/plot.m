293304e2dbe048314ff082119354755b5a801b87
gabrieldbs <gabriel.dbs.1@gmail.com>
2018-11-12 10:14:22 -0300

    Update plot.m

--- src/plot.m
+++ src/plot.m
@@ -1,1 +1,8 @@
+%path = '../../3D/bin/';
+%Energia = load(strcat(path,'Ener_lib_1.txt'));
 
+Energia = load(strcat('Ener_lib_1.txt'));
+figure(1)
+plot(Ener_lib_1(:,2),Ener_lib_1(:,3))
+xlabel('rho_o')
+ylabel('Energy')
