8db50762cb222931f264c3743efb17f3b23a6d21
gabrieldbs <gabriel.dbs.1@gmail.com>
2018-11-12 16:17:33 -0300

    Update plot.m

--- src/plot.m
+++ src/plot.m
@@ -1,8 +1,11 @@
 %path = '../../3D/bin/';
 %Energia = load(strcat(path,'Ener_lib_1.txt'));
-
-Energia = load(strcat('Ener_lib_1.txt'));
-figure(1)
-plot(Ener_lib_1(:,2),Ener_lib_1(:,3))
-xlabel('rho_o')
-ylabel('Energy')
+for i=1:kmax
+  Energia = load(strcat('Ener_lib_%d.txt','i'));
+  figure(1)
+  hold on
+  plot(Ener_lib_1(:,2),Ener_lib_1(:,3))
+  xlabel('rho_o');
+  ylabel('Energy');
+end
+hold off
