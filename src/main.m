5eafb8f9eba41dfe9a7cf5adfa0eb7b6eef67085
celu <gabriel.dbs.1@gmail.com>
2018-11-15 17:31:14 -0300

    algunos arreglos

--- src/main.m
+++ src/main.m
@@ -16,24 +16,27 @@ kd_min=10;
 %---------------------------%
 %-------------------DEFINO CONSTANTES  A CHEQUEAR---------------
 %---------------------------%
-
+kboltz=1; %----buscar es 8.71creo o algo asi-----%
 ka=1;			%---------------DATO--------------------%	
 rho_h=0.5;		%---------------ME PARECE QUE ESTABA VINCULADO CON EL MONOMERO--------------------%
 rho_a=0.5;		%---------------ME PARECE QUE ESTABA VINCULADO CON EL MONOMERO--------------------%
-bet_a=1;		% -------------SOLO FALTA T ---- BETA=1/KT---------%
+Temp=300;  % ------ creo que Kelvin es la unidad correcta ------%
+bet_a=1/(Temp*kboltz);		% -------------SOLO FALTA T ---- BETA=1/KT---------%
 q_a=-1;			%---------------DATO--------------------%
 q_h=1;			%---------------DATO--------------------%
 
 vol_a_menos=1;		%-------------VOLUMENES------------%
 vol_h_mas=1;
 vol_pair=1;		%---------------VOLUMENES-------------------%
+
 muo_a_menos=1;		%--------------------- TODOS ESTOS MU HAY QUE  VER CUALES SON DATO.---.----%%%%%%
 muo_h_mas=1;		%---------------------  Y CUALES HAY QUE RELACIONAR CON EL NUEVO VINCULO--.%%%%%%
 muo_eo=1;
 muo_eop=1;
 muo_a=1;
 muo_ha=1;		% ----------------------------HASTA ACA MU---------------------------------------------%%%%%
-epsi=2;			% VA A SER EL LIMITE DEL CRITERIO A PEDIR DEL FSOLV	
+
+epsi=1;			% VA A SER EL LIMITE DEL CRITERIO A PEDIR DEL FSOLV	
 %%-----------------ALGUNAS DEFINICIONES QUE VOY A USAR DENTRO DEL PROGRAMA--------------%%
 %%Xo va a ser la semilla de la convergencia
 %%X va a ser  la solucion f_a y ps_i
@@ -72,10 +75,25 @@ for i=kd_min:1:kd_max
 			fprintf(fid, 'kd \t rho_o \t Energia Libre\n\n');
 			fprintf(fid, '%f \t %f \t %f\n', m(1),m(2),m(3)) );
 			fclose(fid);
-	%	elseif() aca supongo que si no converge la solucion se puede corregir un par de cosas 
-	%		o la cantidad de iteraciones o redefinir Xo para volver a resolver,
+	%	elseif(epsi < check & check <10*epsi)
+               X= fsolve(Func,Xo); % ACA FALTA AGREGARLE LIMITES A LAS VARIABLES  TODABIA NO SE COMO HACERLO
+               %mi idea es que si esta cerca un orden de magnitud pido que haga mas itrraciones para ver si llega 
+               %  aca supongo que si no converge la solucion se puede corregir un par de cosas 
+	           %  o la cantidad de iteraciones o redefinir Xo para volver a resolver,
+	            Y=funcion_2(X); %%  ACA LA IDEA ES TENER TODAS LAS VARIABLES
+			    %F2=[f_a,psi,rho_h_mas,rho_a_menos,f_ha,f_hap,f_eo,f_eop]
+			     Z=[Y,rho_o];
+			%%---------ELT-------------------%%
+			     Etot=funcion_3(Z);	%% ACA LA IDEA ES QUE LA FUNCION  SUME  TODAS LAS CONTRIBUCIONESEN UNA ETOTAL
+			%%-----------CUANDO SALGO DEL FOR QUIERO QUE AGREGUE UNA LINEA EN EL TXT-----
+			     m=[kd,rho_o,Etot];
+			
+			     fprintf(fid, 'kd \t rho_o \t Energia Libre\n\n');
+			     fprintf(fid, '%f \t %f \t %f\n', m(1),m(2),m(3)) );
+			     fclose(fid);
 	%	else	en caso de que no converja  habría que armar un archivo que diga que para ese kd no hay sol
-	%		
+	%		supongo que sino funciona mejorando la cantidad de iteraciones hay que cambiar las comd iniciales 
+	%         puede ser que para ciertos valores de kd rhoo no hay solucion habria que contemplarlo 
 		end
 		
 	end
