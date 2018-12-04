4e94554868f84d0cf49e5132b6c9b56ab8452467
gabrieldbs <gabriel.dbs.1@gmail.com>
2018-12-04 12:16:42 -0300

    cambio lsqnonlin

--- src/main.m
+++ src/main.m
@@ -5,6 +5,9 @@
 %Defino El maximo , el minimo y el paso del rho_o
 %n=cantidad de iteraciones
 
+pkg load optim %VOY A USAR EN VEZ DE FSOLVE ISQNONLIN PORQUE NO TIENE LIMITES FSOLVE
+
+
 rho_o_min=0.01;
 rho_o_max=0.99;
 nrho=100;
@@ -58,37 +61,27 @@ for i=kd_min:1:kd_max
 		Etot=0;
 		m=0;
 		%		LAS DOS FUNCIONES JUNTAS  COMO FUNC=[EQ1;EQ2]; X(1)=f_a X(2)=ps_i
-		Func =  @(X) [X(1)*(1+rho_h*(exp(-bet_a*X(2)*q_h))/ka)+(kd/ka)*rho_o*X(1)*rho_h*(exp(-bet_a*X(2)*q_h))*(X(1)+X(1)*rho_h*(exp(-bet_a*X(2)*q_h))/ka)-1; rho_a*exp(-bet_a*X(2)*q_a)+rho_o*X(1)+rho_h*exp(-bet_a*X(2)*q_h)-1;]
-		% CONDICIONES INICIALES PARA EL FSOLV Xo=[f_a,ps_i]%
-		Xo=[0.2,0.5];	%%
-		X= fsolve(Func,Xo); % ACA FALTA AGREGARLE LIMITES A LAS VARIABLES  TODABIA NO SE COMO HACERLO
+		Func =  @(X) [X(1)*(1+rho_h*(exp(-bet_a*X(2)*q_h))/ka)+(kd/ka)*rho_o*X(1)*rho_h*(exp(-bet_a*X(2)*q_h))*(X(1)+X(1)*rho_h*(exp(-bet_a*X(2)*q_h))/ka)-1; rho_a*exp(-bet_a*X(2)*q_a)+rho_o*X(1)+rho_h*exp(-bet_a*X(2)*q_h)-1];
+		Xo=[0.2,0.5];	%%% CONDICIONES INICIALES PARA EL FSOLV Xo=[f_a,ps_i]%
+		X= lsqnonlin(Func,Xo,[0 -4],[1 4])%LIMITES [X(1)MIN X(2)MIN],[X(1)MAX X(2)MAX] X(1) ESTA ENTRE (0 1) X(2) NO SE LOS LIMITES
 		check=sqrt(Func(X)(1)^2+Func(X)(2)^2 ) %Tenia pensado chequear si estan dando cero y en caso de que no  iterar mas veces
 		if (check<epsi)
 			%%---DEBERIA TENER PSI Y F_A----------%%
-			%f_a=X(1);
-			%ps_i=X(2);
-			%%------ECUACIONES------------------%%
+			%f_a=X(1);%ps_i=X(2);
 			%% HABRIA QUE CONSIDERAR EL ERROR %
 			Y=funcion_2(X); %%  ACA LA IDEA ES TENER TODAS LAS VARIABLES
 			%F2=[f_a,psi,rho_h_mas,rho_a_menos,f_ha,f_hap,f_eo,f_eop]
 			Z=[Y,rho_o];
 			%%---------ELT-------------------%%
 			Etot=funcion_3(Z);	%% ACA LA IDEA ES QUE LA FUNCION  SUME  TODAS LAS CONTRIBUCIONESEN UNA ETOTAL
-			%%-----------CUANDO SALGO DEL FOR QUIERO QUE AGREGUE UNA LINEA EN EL TXT-----
 			m=[rho_o,Etot];
 			fprintf(fid, '%f \t %f\n', m(1),m(2) );
 			fclose(fid);
 			
-						
-			%NO SE SI VALE LA PENA PERO MI IDEA ES SINO DA BIEN EL FSOLVE  LE PIDO QUE ITERE MAS VECES%
-			%	PARA VER SI ES UNA CUESTION DE CANTIDAD DE ITERACIONES				  %
-			%	Y SINO PENSABA  QUE SE PODRIA MODIFICAR LAS CONDICIONES INICIALES	 	  %
-			
-		elseif(epsi < check && check <100*epsi)
-                	XX= fsolve(Func,X); 
+		else
+                	XX= lsqnonlin(Func,X,[0 -4],[1 4]); 
 			check=sqrt(Func(XX)(1)^2+Func(XX)(2)^2 );
-			
-	           	YY=funcion_2(XX); %% 
+			YY=funcion_2(XX); %% 
 		    	ZZ=[YY,rho_o];
 			Etot=funcion_3(ZZ);	%% ACA LA IDEA ES QUE LA FUNCION  SUME  TODAS LAS CONTRIBUCIONESEN UNA ETOTAL
 			%%-----------CUANDO SALGO DEL FOR QUIERO QUE AGREGUE UNA LINEA EN EL TXT-----
@@ -96,7 +89,6 @@ for i=kd_min:1:kd_max
 			fprintf(fid, '%f \t %f\n', m(1),m(2) );
 			fclose(fid);
 			
-	    	%else	%en caso de que no converja  habría que armar un archivo que diga que para ese kd no hay sol,	
 		end
 	end
 	
