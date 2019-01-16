20bffcf20150e041e4832675db84b3a7ccf356f7
gabrieldbs <gabriel.dbs.1@gmail.com>
2019-01-16 16:19:11 -0300

    ea

--- src/fa_ph.m
+++ src/fa_ph.m
@@ -4,7 +4,7 @@ clear all;
       % ACA EMPIEZO ARMAR EL PROGRAMA QUE ITERE ELIB EN FUNCION RHO_O Y KD
       %-------------------------%
       % DEFINICIONES GLOBALES
-pkg load optim;
+%pkg load optim;
 
 
 global bet_a
@@ -15,9 +15,11 @@ global rho_a_bulk
 global rho_o
 global psi
 global f_A
+global f_ha
+global f_hap
  
 
-rho_o = 0.85 ;  % monomer concentration in film (M); habiamos dicho de poner aprox 1
+rho_o = 0.90 ;  % monomer concentration in film (M); ~ 1
 vpair = 1.0 ;   % CHECKEAr
 Temp=298;       % ------ K ------%
 bet_a=96485/(Temp*8.3144);		% |e|/kT in V^-1
@@ -28,17 +30,17 @@ bet_a=96485/(Temp*8.3144);		% |e|/kT in V^-1
 %%-----------------ALGUNAS DEFINICIONES QUE VOY A USAR DENTRO DEL PROGRAMA--------------%%
 
 epsi=10^(-5);
-phmax=12;
+phmax=60;
 paso=0.2;
-for kdexp=0:1:10
-  kd=10^(kdexp-3);
+for kdexp=1:1:1
+  kd=10^(kdexp);
+  kd=0;
   i=0;
-  for ph=0:paso:phmax
-    pkg load optim
-    i=i+1;
+  for iph=1:1:phmax
+    ph=iph*paso;
     rho_h_bulk=10^(-ph);	
-    rho_a_bulk=10^(-ph);
-    ka=10^(-1);		%% A CHEQUEAR O PREGUNTAR 
+    rho_a_bulk=10^(-3)+10^(-ph);
+    ka=10^(-5);		
     f_A = 0.5;		 %% initial conditions for solver
     psi = 0;		 %% initial conditions for solver
     %lbounds = [0 -inf];	 %% bounds of  f_A and psi
@@ -48,47 +50,60 @@ for kdexp=0:1:10
     Func = @(X) [X(1)*(1+rho_h_bulk*(exp(-bet_a*X(2)))/ka)+(kd/ka)*rho_o*X(1)*rho_h_bulk*(exp(-bet_a*X(2)))*(X(1)+X(1)*rho_h_bulk*(exp(-bet_a*X(2)))/ka)-1;rho_a_bulk*exp(bet_a*X(2))+rho_o*X(1)-rho_h_bulk*exp(-bet_a*X(2))];
     %[X resnorm] = lsqnonlin(@myfun,Xo,lbounds,ubounds, options); 		
     X = fsolve(Func,Xo); 
-    check(kdexp*(phmax/paso+1)+i)=[sqrt(Func(X)(1)^2+Func(X)(2)^2 )] ;
+   
     %myfun(X);     
     %resnorm;	
     psi = X(2);
     f_A = X(1);
-    m(kdexp*(phmax/paso+1)+i)=[X(1)];
-    r(kdexp*(phmax/paso+1)+i)=[X(2)];
+    f_ha = f_A*rho_h_bulk*(exp(-bet_a*psi))/ka;
+    f_hap =1-f_A-f_ha;
+    ph_a(iph)=ph;
+    m(iph,kdexp)=[X(1)];  % to plot f_A vs ph
+    n(iph,kdexp)=[f_ha];  % to plot f_ha vs ph
+   % o(iph,kdexp)=[f_hap];  % to plot f_hap vs ph
+    check(iph,kdexp)=[sqrt(Func(X)(1)^2+Func(X)(2)^2 )] ;
+    r(iph,kdexp)=[X(2)];  % to plot  psi vs ph
     %fprintf(fid, '%f \t %f\n', m(1),m(2) );		
   end
+
+   hold all;
+  plot (ph_a(1,:),m(:,kdexp)',";f_{A};")
+  plot (ph_a(1,:),n(:,kdexp)',";f_{ha};")
+  %plot (ph_a(1,:),o(:,kdexp)',";f_{hap};")
+  plot (ph_a(1,:),check(:,kdexp)',";check;")
+  hold all;
 end
 
 
-fid = fopen('fa_ph.txt',"w");%aca no se si va w o s o sino va nada 
- for kdexp=0:1:10
- i=0;
-  for ph=0:paso:phmax
-    i=i+1;
-    fprintf(fid, '%f \t %f\n', ph,m(kdexp*(phmax/paso+1)+i) );
-  end
- end
-fclose(fid);
+%fid = fopen('fa_ph.txt',"w");%aca no se si va w o s o sino va nada 
+ %for kdexp=0:1:10
+ %i=0;
+ % for ph=0:paso:phmax
+ %   i=i+1;
+ %   fprintf(fid, '%f \t %f\n', ph,m(kdexp*(phmax/paso+1)+i) );
+ % end
+ %end
+%fclose(fid);
 
-fide = fopen('psi_ph.txt',"w");%aca no se si va w o s o sino va nada 
- for kdexp=0:1:10
- i=0;
-  for ph=0:paso:phmax
-    i=i+1;
-    fprintf(fide, '%f \t %f\n', ph,r(kdexp*(phmax/paso+1)+i) );
-  end
- end
-fclose(fide);
+%fide = fopen('psi_ph.txt',"w");%aca no se si va w o s o sino va nada 
+ %for kdexp=0:1:10
+ %i=0;
+ % for ph=0:paso:phmax
+  %  i=i+1;
+   % fprintf(fide, '%f \t %f\n', ph,r(kdexp*(phmax/paso+1)+i) );
+  %end
+ %end
+%fclose(fide);
 
-fidch = fopen('check_ph.txt',"w");%aca no se si va w o s o sino va nada 
- for kdexp=0:1:10
- i=0;
-  for ph=0:paso:phmax
-    i=i+1;
-    fprintf(fidch, '%f \t %f\n', ph,check(kdexp*(phmax/paso+1)+i) );
-  end
- end
-fclose(fidch);
+%fidch = fopen('check_ph.txt',"w");%aca no se si va w o s o sino va nada 
+% for kdexp=0:1:10
+% i=0;
+%  for ph=0:paso:phmax
+ %   i=i+1;
+ %   fprintf(fidch, '%f \t %f\n', ph,check(kdexp*(phmax/paso+1)+i) );
+ % end
+ %end
+%fclose(fidch);
 clear all
 %%------------------funciones para fsolve--------------------%%%
 %function  F = myfun(X)
